8c87de923aff4ad8c336a489ed247baef7cd5f1e
Adrien Riguet <adrien.riguet@hotmail.com>
2023-01-19 16:08:38 +0100

    first_commit

--- fao_git.py
+++ fao_git.py
@@ -1,11 +1,14 @@
 import pandas as pd
 
+
 def grp1(dir):
     #Group 1, réponse à la question X : calcul...
     print("Résultat Q1:...")
+
 def grp2(dir):
     #Group 2, réponse à la question X : calcul...
     print("Résultat Q2:...")
+
 def grp3(dir):
     #Group 3, réponse à la question X : calcul...
     print("Résultat Q3:...")
