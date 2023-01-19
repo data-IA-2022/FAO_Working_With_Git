fb4096692aca033443df39892298a7a6ce249bd8
Maud <m-aud.b@live.fr>
2023-01-19 16:40:20 +0100

    question n°6

--- fao_git.py
+++ fao_git.py
@@ -18,9 +18,16 @@ def grp5(dir):
     #Group 5, réponse à la question X : calcul...
     print("Résultat Q5:...")
 
-def grp6(dir):
+def question_6(aliments_df):
     #Group 6, réponse à la question X : calcul...
-    print("Résultat Q6:...")
+    aliments_df.drop(aliments_df[aliments_df['Pays'] == 'Chine'].index, inplace=True)
+    aliments_df["dispo_interieure_alimentaire_kg"] = aliments_df["Disponibilité intérieure"]*1000000
+    aliments_df["dispo_interieure_alimentaire_kcal"] = aliments_df["dispo_interieure_alimentaire_kg"]*aliments_df["ratio_kcal/kg"]
+    dispo_mondiale_vegetaux_df = aliments_df[aliments_df['Origine'] == 'Végétale']
+    dispo_mondiale_vegetaux_df[['Pays', 'Origine', 'Produit', 'dispo_interieure_alimentaire_kcal']]
+    dispo_mondiale_vegetaux_df_sum = dispo_mondiale_vegetaux_df[['dispo_interieure_alimentaire_kcal']].sum()
+    dispo_mondiale_vegetaux_df_sum
+    print(dispo_mondiale_vegetaux_df_sum)
 
 def grp7(dir):
     #Group 7, réponse à la question X : calcul...
