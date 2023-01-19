8e9c91c6aa6d31f8cbcb2152ce07bfda0a1708b3
JeremyMikaleff <120096690+JeremyMikaleff@users.noreply.github.com>
2023-01-19 16:44:14 +0100

    testAvecExo1

--- fao_git.py
+++ fao_git.py
@@ -24,7 +24,40 @@ def grp6(dir):
 
 def grp7(dir):
     #Group 7, réponse à la question X : calcul...
-    print("Résultat Q7:...")
+    #--------------------------------- Nombre d'humains sur la planète (question 1)------------------------------------
+    import pandas as pd
+    import os
+    import glob
+    #----------------------------- Récupération des fichier dans un dataframe -----------------------------------------
+    fns=glob.glob('fao_2013/*.csv', recursive=True)
+
+    df_bigTab = pd.DataFrame(columns=['Code Domaine', 'Domaine', 'Code Pays', 'Pays', 'Code Élément',
+        'Élément', 'Code Produit', 'Produit', 'Code Année', 'Année', 'Unité',
+        'Valeur', 'Symbole', 'Description du Symbole'])
+
+
+    nameModificateur=['Code Domaine', 'Domaine', 'Code Pays', 'Pays', 'Code Élément',
+        'Élément', 'Code Produit', 'Produit', 'Code Année', 'Année', 'Unité',
+        'Valeur', 'Symbole', 'Description du Symbole']
+    #-------------------------- Construction du big Tableau avec tous les fichiers-------------------------------------
+    for fn in fns:
+        fns = fn[13:-4]
+        df=pd.read_csv(fn, names=nameModificateur, header=0)
+        print("\n")  
+        print(f"{Fore.YELLOW}---------------------- Récupération du fichier {fns} --------------------{Style.RESET_ALL}\n")
+        print(f"- {fn:50s} ({df.shape[0]:6d}, {df.shape[1]:2d}) - {Fore.BLACK}{Back.CYAN}{fns}{Style.RESET_ALL} \n")
+        print(f"{Fore.YELLOW}------------------------------------------------------------------------------{Style.RESET_ALL}\n")
+        print("\n")
+        print(f"{Fore.YELLOW}------------------------ Analyse du fichier {fns} ------------------------{Style.RESET_ALL}\n")
+        print(f"{df.describe()}")
+        print("\n")
+        print(f"{Fore.YELLOW}-------------------------------------------------------------------------------{Style.RESET_ALL}\n")
+        df_bigTab=df_bigTab.append(df)
+        pop_planetaire = df_pop['Valeur'].sum()
+        print (f"La population mondiale en 2013 est de : {pop_planetaire}")
+        df_pop = df_pop.loc[df_pop["Pays"] != "China"]
+        pop_planetaire = df_pop['Valeur'].sum()
+        print (f"La population mondiale en 2013 est de : {pop_planetaire}")
 
 
 #..................................
