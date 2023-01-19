5324594f4c931e1343b8525b8b80f97428eb0e2b
ClemHN <clementine.henri@gmail.com>
2023-01-19 16:15:12 +0100

    commit du groupe Adile, Clémentine et David

--- fao_git.py
+++ fao_git.py
@@ -1,8 +1,28 @@
 import pandas as pd
 
+
 def grp1(dir):
+    dir = "fao_2013/FAOSTAT_2013_population.csv"
     #Group 1, réponse à la question X : calcul...
     print("Résultat Q1:...")
+    #chemin du fichier population qui est dans le meme dossier 
+    #lecture du fichier csv
+    df_pop = pd.read_csv(dir)
+    #print(df_pop)
+    #isolation de la collone d'intérêt
+    df_popVal = df_pop[['Value']]
+    #calcul de la somme des populations des pays du fichier
+    df_popSum = df_popVal.sum()*1000 # *1000 car unité est par miller d'individus
+    #print(df_popSum)
+    # somme des populations est de 8413993000 ... en 2013 ce qui semble élevé
+    #drop Chine car plusieurs référence
+    df_pop_update = df_pop.drop(df_pop[(df_pop['Country Code']) & (df_pop['Country'] == 'China')].index, inplace=True)
+    #isolation de la collone d'intérêt
+    df_popVal2 = df_pop[['Value']]
+    #calcul de la somme des populations des pays du fichier
+    df_popSum = df_popVal2.sum()*1000 # *1000 car unité est par miller d'individus
+    print(df_popSum) 
+
 def grp2(dir):
     #Group 2, réponse à la question X : calcul...
     print("Résultat Q2:...")
