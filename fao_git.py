3844bc195961b28386989908a8cfd7791b69bb70
Tarik37 <tarik.medjahed@yahoo.fr>
2023-01-19 15:02:53 +0100

    premier commit

---
+++ fao_git.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+def grp1(dir):
+    #Group 1, réponse à la question X : calcul...
+    print("Résultat Q1:...")
+def grp2(dir):
+    #Group 2, réponse à la question X : calcul...
+    print("Résultat Q2:...")
+def grp3(dir):
+    #Group 3, réponse à la question X : calcul...
+    print("Résultat Q3:...")
+
+def grp4(dir):
+    #Group 4, réponse à la question X : calcul...
+    print("Résultat Q4:...")
+
+def grp5(dir):
+    #Group 5, réponse à la question X : calcul...
+    print("Résultat Q5:...")
+
+def grp6(dir):
+    #Group 6, réponse à la question X : calcul...
+    print("Résultat Q6:...")
+
+def grp7(dir):
+    #Group 7, réponse à la question X : calcul...
+    print("Résultat Q7:...")
+
+
+#..................................
+
+print("Projet commun")
+dir='fao_2013'
+grp1('dir')
