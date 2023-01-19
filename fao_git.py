9b04cbd2786340267e46d44629202d6fc7089427
lysvallee <evelyne.theodose@gmail.com>
2023-01-19 16:25:41 +0100

    Création de la fonction

--- fao_git.py
+++ fao_git.py
@@ -1,11 +1,18 @@
+import numpy as np
 import pandas as pd
+import sqlite3
+from glob import glob
+from os import chdir, path
 
 def grp1(dir):
     #Group 1, réponse à la question X : calcul...
     print("Résultat Q1:...")
 def grp2(dir):
-    #Group 2, réponse à la question X : calcul...
-    print("Résultat Q2:...")
+    df_population = pd.read_csv(dir+'/FAOSTAT_2013_population.csv')
+    # On retire la Chine qui est en doublon
+    df_population.query('Country != "China"', inplace=True)
+    poptot = df_population['population'].sum() * 1000
+    print(f"Résultat Q2: {poptot}")
 def grp3(dir):
     #Group 3, réponse à la question X : calcul...
     print("Résultat Q3:...")
@@ -26,9 +33,6 @@ def grp7(dir):
     #Group 7, réponse à la question X : calcul...
     print("Résultat Q7:...")
 
-def grpMaud_Lorenzo(dir):
-    #Group 7, réponse à la question X : calcul...
-    print("Résultat Q7:...")
 
 #..................................
 
